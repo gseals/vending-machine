8e0d51051339fb0e7e6e22dea347b55552ccb8e5
Gabriel Seals <sealsgj@gmail.com>
2019-10-29 21:39:45 -0500

    mering for the night

---
+++ src/javascripts/components/Auth/auth.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import util from '../../helpers/utilities';
+import monkeyButt from './googlebutton.png';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
+const loginButton = () => {
+  const domString = `<button id="google-auth" class="btn btn-danger">
+  <img src="${monkeyButt}" />
+  </button>`;
+  util.printToDom('auth', domString);
+  $('#google-auth').click(signMeIn);
+};
+
+export default { loginButton };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 import apiKeys from './helpers/apiKeys.json';
+import auth from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/MyNavbar/myNavbar';
 import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginButton();
+  authData.checkLoginStatus();
+  myNavbar.logoutEvent();
 };
 
 init();
