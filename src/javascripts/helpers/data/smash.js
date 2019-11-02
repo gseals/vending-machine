29413cd0bfe0e4c970860f8ec259067c4b19257f
Gabriel Seals <sealsgj@gmail.com>
2019-11-02 13:10:31 -0500

    all works

--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,10 +1,20 @@
 import machineData from './machineData';
 import positionData from './positionData';
+import snackPositionData from './snackPositionData';
+import snackData from './snackData';
 
 const getCompleteMachine = () => new Promise((resolve, reject) => {
   machineData.getMachine()
     .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
-    .then((positions) => resolve(positions))
+    .then((positions) => {
+      snackPositionData.getAllSnackPositionsByMachineId(positions[0].machineId)
+        .then((snackPositions) => {
+          snackData.getSnacksByUid(positions[0].uid).then((snacks) => {
+            console.log('snackPositions', snackPositions);
+            resolve(snacks);
+          });
+        });
+    })
     .catch((error) => reject(error));
   // first, getMachines - returns first machine(hard coding)
   // second, use machineId to get all positions for that machine
