4843e7aa31f61d25c60602c84f71b619ccf17331
Gabriel Seals <sealsgj@gmail.com>
2019-11-05 20:35:09 -0600

    all works

--- src/javascripts/components/machine/machine.js
+++ src/javascripts/components/machine/machine.js
@@ -1,7 +1,18 @@
+import $ from 'jquery';
 import smash from '../../helpers/data/smash';
 import util from '../../helpers/utilities';
 import snackComponent from '../snacks/snacks';
 import './machine.scss';
+import snackData from '../../helpers/data/snackData';
+
+const buySnack = (e) => {
+  e.stopImmediatePropagation();
+  const snackId = e.target.id.split('buy-')[1];
+  snackData.buySnack(snackId)
+    // eslint-disable-next-line no-use-before-define
+    .then(() => buildTheMachine())
+    .catch((error) => console.error(error));
+};
 
 const buildTheMachine = () => {
   smash.getCompleteMachine()
@@ -13,6 +24,7 @@ const buildTheMachine = () => {
       });
       domString += '</div>';
       util.printToDom('machine', domString);
+      $('#machine').on('click', '.buy-snack', buySnack);
     })
 
     .catch((error) => console.error(error));
