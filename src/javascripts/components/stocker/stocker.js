53d445320d931d13bc5233e6d9d72e0ad80c6e38
Gabriel Seals <sealsgj@gmail.com>
2019-11-05 18:52:50 -0600

    modal works

--- src/javascripts/components/stocker/stocker.js
+++ src/javascripts/components/stocker/stocker.js
@@ -7,6 +7,7 @@ import utilities from '../../helpers/utilities';
 import stockCard from '../StockCard/stockCard';
 import snackPositionData from '../../helpers/data/snackPositionData';
 import machine from '../machine/machine';
+import snackData from '../../helpers/data/snackData';
 
 const deleteFromMachine = (e) => {
   e.preventDefault();
@@ -44,11 +45,34 @@ const addToMachine = (e) => {
     .catch((error) => console.error(error));
 };
 
+const addNewSnack = (e) => {
+  e.stopImmediatePropagation();
+  const { uid } = firebase.auth().currentUser;
+  const newSnack = {
+    imageUrl: $('#snack-image-url').val(),
+    name: $('#snack-name').val(),
+    price: $('#snack-price').val() * 1,
+    currentStocked: 0,
+    lifetimeNum: 0,
+    uid,
+  };
+  snackData.addNewSnack(newSnack)
+    .then(() => {
+      $('#exampleModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      buildTheStocker(uid);
+    })
+    .catch((error) => console.error(error));
+};
+
 const buildTheStocker = (uid) => {
   smash.getSnacksWithPositions(uid)
     .then((snacks) => {
       let domString = '<h2>STOCK THE MACHINE</h2>';
+      domString += `<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Add Snack </button>`;
       domString += '<div class="d-flex flex-wrap">';
+
       snacks.forEach((snack) => {
         domString += stockCard.makeASnack(snack);
       });
@@ -56,6 +80,7 @@ const buildTheStocker = (uid) => {
       utilities.printToDom('stock', domString);
       $('#stock').on('click', '.delete-snack-position', deleteFromMachine);
       $('#stock').on('click', '.add-snack-position', addToMachine);
+      $('#add-new-snack').click(addNewSnack);
     })
     .catch((error) => console.error(error));
 };
